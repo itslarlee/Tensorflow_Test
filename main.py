cac25c1cd710cc3be679d3e8659a7e6d1a357354
Leeren <leerenwusegura@gmail.com>
2020-08-05 20:38:00 -0600

    Changed some things, still not well commented

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from sklearn.utils import shuffle
 
 data = pd.read_csv("student-mat.csv", sep=";")
 
-print(data.head())
+# print(data.head())
 
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
 
-print(data.head())
+# print(data.head())
 
 # tambien se llama label
 
@@ -27,14 +27,18 @@ x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y
 
 linear = linear_model.LinearRegression()
 
+# Reminder for Lee: Estudia las librerias de numpy y sklearn
+
 linear.fit(x_train, y_train)
 accuracy = linear.score(x_test, y_test)
-print(accuracy)
+# print(accuracy)
 
+
+# Estudiar que significa los datos de aqui abajo(Co e Intercept)
 print("Co: \n", linear.coef_)
 print("Intercept: \n", linear.intercept_)
 
 predictions = linear.predict(x_test)
 
-for x in range (len(predictions)):
-    print(predictions[x], x_test[x], y_test[x])
+for x in range(len(predictions)):
+    print("Datos: ", x_test[x], " ||| Prediccion: ", predictions[x], " ||| Valor real: ", y_test[x])
